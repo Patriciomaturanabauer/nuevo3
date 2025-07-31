2f132b91843c3c63d05bc6cfb2fd1b8f8b18265c
Patriciomaturanabauer <patomat@yahoo.com>
2025-07-30 22:05:47 -0400

    agrego archivo iterog

---
+++ iterog.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 27 15:09:32 2025
+
+@author: patom
+"""
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+salad = input('ingrese nombre')
+edad = int(input('ingrese edad'))
+
+numero = int(input('ingrese el otro dato'))
+
+igua = numero+edad
+
+igua
+
